ef3331115847bc9c02376241ff230a130dc7615c
Claas Nieslony <bb@nieslony.at>
2017-11-09 16:47:43 +0100

    Ignore files generated by auto tools

--- gen-auto.sh
+++ gen-auto.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 echo --- Removing old files... ---
-find -maxdepth 1 -type l -delete -print
+rm -rvf .gen-auto.sh.kate-swp \
+        Makefile.in \
+        aclocal.m4 \
+        ar-lib \
+        autom4te.cache/ \
+        compile \
+        config.guess \
+        config.sub \
+        configure \
+        depcomp \
+        install-sh \
+        ltmain.sh \
+        m4/ \
+        missing \
+        src/Makefile.in \
 
-rm -rfv aclocal.m4 autom4te.cache m4 configure  Makefile.in config.status  
-rm -rfv Makefile  config.h  stamp-h1  libtool  config.log build plugins
 
 if [ "$1" = "clean" ]; then
     exit
@@ -20,4 +32,4 @@ echo ---autoconf ---
 autoconf
 
 echo --- automake ---
-automake --add-missing
+automake --add-missing --copy
