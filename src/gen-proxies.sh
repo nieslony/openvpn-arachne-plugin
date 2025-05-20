a4b496f92f893c390ee8a58c08caad26246a4ea1
Claas Nieslony <github@nieslony.at>
2025-05-20 18:19:28 +0200

    Add proxy files for sdbus-cpp 2.x

--- src/gen-proxies.sh
+++ src/gen-proxies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 declare -A dbus_proxies=(
     ["FirewallD1_Config_IpSet_Proxy.h"]="/org/fedoraproject/FirewallD1/config/ipset/0"
     ["FirewallD1_Config_Policy_Proxy.h"]="/org/fedoraproject/FirewallD1/config/policy/0"
@@ -8,10 +7,10 @@ declare -A dbus_proxies=(
     ["FirewallD1_Proxy.h"]="/org/fedoraproject/FirewallD1"
 )
 
-if [ -e /usr/lib64/libsdbus-c++.so.1 ]; then
-	SDBUS_VERSION=1
-elif [ -e /usr/lib64/libsdbus-c++.so.2 ]; then
+if [ -e /usr/lib64/libsdbus-c++.so.2 ]; then
 	SDBUS_VERSION=2
+elif [ -e /usr/lib64/libsdbus-c++.so.1 ]; then
+	SDBUS_VERSION=1
 else
 	echo "Cannot detect sdbus version."
 	exit 1
