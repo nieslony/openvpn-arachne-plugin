ba551b13d6545cd274f8d6c8681c17654008a6ee
Claas Nieslony <github@nieslony.at>
2026-08-13 19:02:19 +0200

    Improve podman build

--- build.sh
+++ build.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
-. /etc/os-release
-PLATFORM="${PLATFORM_ID/platform:/}"
-if [ -z "$PLATFORM" ]; then
-    PLATFORM="$ID:$VERSION_ID"
-fi
-RPM_DIR="/build/repos/$PLATFORM"
+set -eu
 
-echo "--- Prepare Environment ---"
+. /etc/os-release
+
+if [ -z "${PLATFORM_ID+x}" ]; then
+    PLATFORM=$ID:$VERSION_ID
+else
+    PLATFORM=${PLATFORM_ID/platform:/}
+fi
+
+RPM_DIR="/repos/$PLATFORM"
+
+function log {
+    echo "--- $@ ---"
+}
+
+function create_tar {
+    mkdir -p /build-tar
+    dnf install -y rsync
+    rm -rf /build-tar
+    rsync -av /source/ /build-tar
+    pushd /build-tar
+
+    log "Spec file is missing, try to generate from .spec.in"
+    VERSION_INFO=$(
+        cat configure.ac \
+            | grep "^AC_INIT" \
+            | grep -o '\[[a-z0-9.\-]*\]' \
+            | tr -d '[]' \
+            | tr '\n' ' '
+    )
+    PROG_NAME=$( echo $VERSION_INFO | cut -d ' ' -f1 )
+    PROG_VERSION=$( echo $VERSION_INFO | cut -d ' ' -f2 )
+    cat *spec.in \
+        | sed -E "s/@PACKAGE_NAME@/$PROG_NAME/ ; s/@PACKAGE_VERSION@/$PROG_VERSION/" \
+        > $PROG_NAME.spec
+    popd
+}
+
+log "Prepare Environment"
 mkdir -pv "$RPM_DIR"
 
-for i in $ID_LIKE $ID ; do
+for i in ${ID_LIKE:-} ${ID:-} ; do
     case $i in
         rhel|fedora)
             family=rhel
@@ -32,8 +64,11 @@ if [ -z "$family" ]; then
 fi
 
 if [ "$family" == "rhel" ]; then
-    echo "--- Install rpmbuild ----"
-    dnf install -y rpm-build which
+    if grep -q "^proxy=" /etc/dnf/dnf.conf ; then
+        sed -i "s#^proxy=.*#proxy=${http_proxy}#" /etc/dnf/dnf.conf
+    else
+        echo "proxy=${http_proxy}" >> /etc/dnf/dnf.conf
+    fi
 
     if [ "$ID" = "almalinux" ]; then
         echo "--- Install almalinux extras ---"
@@ -42,8 +77,28 @@ if [ "$family" == "rhel" ]; then
         dnf install -y epel-release
     fi
 
-    echo "--- Install Build Dependencies ---"
-    dnf builddep -y /build/*spec
+    log "Install rpmbuild"
+    dnf install -y rpm-build which createrepo
+
+    SPEC_FILE="$( ls *spec 2> /dev/null ; true )"
+    SPEC_IN_FILE="$( ls *spec.in 2> /dev/null ; true )"
+    if [ -z "$SPEC_FILE" -a -n "$SPEC_IN_FILE" ]; then
+        create_tar
+    fi
+
+    log "Install Build Dependencies"
+    dnf builddep -y /build-tar/*spec
+
+    TAR_FILE="$( ls *.tar.* ; true )"
+    if [ -z "$TAR_FILE" ]; then
+        pushd /build-tar
+        log "Tar file does not exist, running 'make dist'"
+        dnf install -y automake libtool autoconf-archive autoconf-latest
+        ./gen-auto.sh
+        ./configure
+        make dist
+        popd
+    fi
 elif [ "$family" == "suse" ]; then
     zypper install -y rpm-build gawk
     zypper install -y $( rpmbuild -td *gz 2>&1 | awk '/is needed/ { print $1; }' )
@@ -52,11 +107,14 @@ elif [ "$family" == "debian" ]; then
     apt install -y dh-make devscripts
 fi
 
+TAR_FILE=$( ls -1 /source/*tar.gz /build-tar/*tar.gz 2>/dev/null | head -1 ; true )
+
 if [ -n "$( which rpm )" ]; then
-    echo "--- Build RPM ---"
-    rpmbuild -tb /build/*tar.gz --define "_rpmdir $RPM_DIR"
+    log "Build RPM"
+    rpmbuild -tb $TAR_FILE --define "_rpmdir $RPM_DIR"
+    createrepo $RPM_DIR
 elif [ -n "$( which apt)" ]; then
-    echo "Build DEB ---"
+    log "Build DEB"
     mkdir ~/DEBIAN
     cat *spec | awk '
         /^Name:/    { print "Package:     " $2; }
@@ -68,4 +126,3 @@ elif [ -n "$( which apt)" ]; then
         }
         ' | tee ~/DEBIAN/control
 fi
-
--- podman-build.sh
+++ podman-build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -eu
+
 . /etc/os-release
 SRC_DIR=$( dirname $0 )
+
+REPO_DIR=$PWD/repos
+if [ ! -e "$REPO_DIR" ]; then
+    mkdir -v $REPO_DIR
+fi
 
 if [ -z "$1" ]; then
     BUILD_DISTRO="$ID:$VERSION_ID"
@@ -12,21 +19,19 @@ fi
 if netstat -tln | grep 3128 -q ; then
     my_ip="$( host $HOSTNAME | awk '{ print $NF; }' | head -1 )"
     HTTP_PROXY=http://$my_ip:3128
-    echo "Enable local proxy $HTTP_PROXY"
-else
-    echo "No local proxy found"
 fi
-
 
 echo "=== Build for disto $BUILD_DISTRO ==="
 HTTP_PROXY=$HTTP_PROXY http_proxy=$HTTP_PROXY \
     podman run \
         -ti \
         --rm \
-        --workdir=/build \
-        --volume $SRC_DIR:/build:Z \
+        --workdir=/source \
+        --volume $SRC_DIR:/source:ro,Z \
+        --volume $REPO_DIR:/repos:Z \
         --network=host \
         --http-proxy \
+        --env HTTP_PROXY=$HTTP_PROXY \
+        --env http_proxy=$HTTP_PROXY \
         $BUILD_DISTRO \
         ./build.sh
-
